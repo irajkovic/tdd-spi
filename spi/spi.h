90648d1d582cd3c5596d1b947e2da42aaf3acd7a
Ivan Rajkovic <iivanrajkovic@gmail.com>
2016-11-10 07:56:46 -0500

    Test fail for SPI control register

---
+++ spi/spi.h
@@ -0,0 +1,16 @@
+#ifndef SPI
+#define SPI
+
+/****** ATMEGA328p pin definitions  ******/
+
+/* Control register */
+#define SPR0 0
+#define SPR1 0
+#define CPHA 0
+#define CPOL 0
+#define MSTR 0
+#define DORD 0
+#define SPE 0
+#define SPIE 0
+
+#endif
