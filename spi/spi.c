e4169624944f32bf3d8f67767f9e262a8b718a40
Ivan Rajkovic <iivanrajkovic@gmail.com>
2016-11-10 08:42:56 -0500

    Test pass for SPI init

--- spi/spi.c
+++ spi/spi.c
@@ -5,6 +5,5 @@ void SPI_MasterInit(	volatile uint8_t* DDR,		/* SPI port data direction */
 						volatile uint8_t* SPCR,		/* config */
 						volatile uint8_t* SPSR		/* status */)
 {
-	
-
+	*DDR = (1<<DD_MOSI) | (1<<DD_SCK);
 }
