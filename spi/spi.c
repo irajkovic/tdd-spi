2ce2b157329ddb68c6b94c662cdc8ecd55f836b4
Ivan Rajkovic <iivanrajkovic@gmail.com>
2016-11-11 04:33:57 -0500

    Test pass for byte send

--- spi/spi.c
+++ spi/spi.c
@@ -1,5 +1,7 @@
 #include "spi.h"
 
+static struct Registers HAL;
+
 void SPI_MasterInit(	volatile uint8_t* DDR,		/* SPI port data direction */
 						volatile uint8_t* SPDR,		/* data */
 						volatile uint8_t* SPCR,		/* config */
@@ -7,9 +9,12 @@ void SPI_MasterInit(	volatile uint8_t* DDR,		/* SPI port data direction */
 {
 	*DDR = PIN(DD_MOSI) | PIN(DD_SCK);	
 	*SPCR = BIT(SPE) | BIT(MSTR) | BIT(SPR0);
+	
+	HAL.SPDR = SPDR;
+	HAL.SPSR = SPSR;
 }
 
 void SPI_SendByte(const uint8_t byte)
 {
-
+	*(HAL.SPDR) = byte;
 }
